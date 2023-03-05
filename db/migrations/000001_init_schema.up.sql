bf9124d02032dc2ab38ca730136b8ee514e3341c
HudYuSa <hudjempol@gmail.com>
2023-03-05 10:06:58 +0700

    add sqlc config and make user query sql

--- db/migrations/000001_init_schema.up.sql
+++ db/migrations/000001_init_schema.up.sql
@@ -1,15 +1,15 @@
-CREATE TABLE "users" (
-  "id" uuid NOT NULL DEFAULT (uuid_generate_v4 ()),
-  "name" varchar NOT NULL,
-  "email" varchar NOT NULL,
-  "photo" varchar NOT NULL,
-  "verified" boolean NOT NULL,
-  "password" varchar NOT NULL,
-  "role" varchar NOT NULL,
-  "created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
+CREATE TABLE
+    "users" (
+        "id" uuid NOT NULL DEFAULT (uuid_generate_v4 ()),
+        "name" varchar NOT NULL,
+        "email" varchar NOT NULL,
+        "photo" varchar NOT NULL,
+        "verified" boolean NOT NULL,
+        "password" varchar NOT NULL,
+        "role" varchar NOT NULL,
+        "created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "updated_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+    );
 
 CREATE UNIQUE INDEX "users_email_key" ON "users" ("email");
-
