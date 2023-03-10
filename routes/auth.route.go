ff86032aecc615f5753600cf447d0af4eeb337b2
HudYuSa <hudjempol@gmail.com>
2023-03-10 15:17:46 +0700

    update the auth route

--- routes/auth.route.go
+++ routes/auth.route.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"github.com/HudYuSa/sqlc-crud-api-gin/controllers"
+	db "github.com/HudYuSa/sqlc-crud-api-gin/db/sqlc"
+	"github.com/HudYuSa/sqlc-crud-api-gin/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,11 +13,13 @@ type AuthRoutes interface {
 
 type authRoutes struct {
 	authController controllers.AuthController
+	db             *db.Queries
 }
 
-func NewAuthRoutes(authController controllers.AuthController) AuthRoutes {
+func NewAuthRoutes(authController controllers.AuthController, db *db.Queries) AuthRoutes {
 	return &authRoutes{
 		authController: authController,
+		db:             db,
 	}
 }
 
@@ -23,4 +27,7 @@ func (ar *authRoutes) SetupRoutes(rg *gin.RouterGroup) {
 	router := rg.Group("/auth")
 
 	router.POST("/register", ar.authController.SignUpUser)
+	router.POST("login", ar.authController.SignInUser)
+	router.GET("/refresh", ar.authController.RefreshAccessToken)
+	router.GET("/logout", middleware.DeserializeUser(ar.db), ar.authController.LogoutUser)
 }
